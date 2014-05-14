40a5bf880d1a557f2d22e9f69eb4f2c380ad5377
Emi2014 <emilia.kolarova@gmail.com>
2014-05-14 14:36:23 +0300

    Initial checkin of sales tool.

---
+++ Socoban/src/Drawer.java
@@ -0,0 +1,12 @@
+import java.awt.Menu;
+
+
+	abstract class Drawer {
+
+	 abstract void drawMenu() ;
+	 abstract void drawScores();
+	 abstract void drawGameField();
+	 abstract void drawStatusLine();
+	}
+
+
---
+++ Socoban/src/StatusLine.java
@@ -0,0 +1,57 @@
+
+public class StatusLine {
+
+	private String playerName;
+	private int level;
+	private String moves;
+	private int time;
+		
+
+    public String getPlayerName() {
+
+        return this.playerName;
+
+    }
+
+    public void setPlayerName(String playerName) {
+
+        this.playerName = playerName;
+
+    }
+
+    public int getLevel() {
+
+        return this.level;
+
+    }
+
+    public void setLevel(int level) {
+
+        this.level = level;
+
+    }
+    public String getMoves() {
+
+        return this.moves;
+
+    }
+
+    public void setMoves(String moves) {
+
+        this.moves = moves;
+
+    }
+
+    public int getTime() {
+
+        return this.time;
+
+    }
+
+    public void setTime(int time) {
+
+        this.time = time;
+
+    }
+
+}
